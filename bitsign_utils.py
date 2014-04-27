6a76bdf4f1a3dcd5b9679dffa029bce482f20dd8
LaurentMT <laurent.salat@bbox.fr>
2014-04-27 19:20:24 +0200

    Rewrote verify_sign_addr() and refactoring
    - Rewrote code for function verify_sign_addr() :
    Initial code, based on pure pycoin functions, introduced a flaw allowing
    potential MITM attacks (attacker modifying the address present in
    response sent by the client)
    
    - Refactorization of code related to orderlen()

--- bitsign_utils.py
+++ bitsign_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-Version: 0.0.1
+Version: 0.0.2
 Utils functions to deal with signatures, addresses and signed messages
 Code strongly inspired by:
 - ELECTRUM:
@@ -17,13 +17,15 @@ Requires:
 import binascii
 import base64
 from pycoin.ecdsa.secp256k1 import generator_secp256k1
-from pycoin.ecdsa.ecdsa import possible_public_pairs_for_signature
+from pycoin.ecdsa.ecdsa import verify
 from pycoin.encoding import double_sha256, public_pair_to_bitcoin_address,\
                             is_valid_bitcoin_address, from_bytes_32, byte_to_int
+from pycoin.ecdsa import numbertheory, ellipticcurve
 
 # Elliptic curves
 G = generator_secp256k1
 N = G.order()
+N_LEN = (1 + len("%x" % N)) // 2 
 
 # Python 2 / 3 compatibility functions
 def to_bytes(x): return x if bytes == str else x.encode()
@@ -34,26 +36,21 @@ def to_bytes(x): return x if bytes == str else x.encode()
 '''
 Signatures
 '''
-def orderlen(n):
-    return (1 + len("%x" % n)) // 2 
-
-def string_to_number(s, n):
-    length = orderlen(n)
-    if len(s) != length: raise BaseException("Wrong signature length")
+def string_to_number(s):
+    if len(s) != N_LEN: raise BaseException("Wrong signature length")
     return int(binascii.hexlify(s), 16)
 
-def sig_decode_string(sign, n):
-    length = orderlen(n)
-    if len(sign) != 2 * length: raise BaseException("Wrong signature length")
-    r = string_to_number(sign[:length], n)
-    s = string_to_number(sign[length:], n)
+def sig_decode_string(sign):
+    if len(sign) != 2 * N_LEN: raise BaseException("Wrong signature length")
+    r = string_to_number(sign[:N_LEN])
+    s = string_to_number(sign[N_LEN:])
     return r,s
 
 def decode_sig(sign):
     ds = base64.b64decode(sign) 
     if len(ds) != 65: raise BaseException("Wrong encoding")
     v = byte_to_int(ds[0])
-    r,s = sig_decode_string(ds[1:], N)
+    r,s = sig_decode_string(ds[1:])
     return v,r,s
 
 '''
@@ -67,14 +64,43 @@ def int_sig_hash(msg):
 Validation of message, signature, address
 '''
 def verify_sign_addr(msghash, vrs, addr, is_test=False):
+    '''
+    Inspiration from :
+     - possible_public_pairs_for_signature() in pycoin.ecdsa.ecdsa
+     - https://github.com/michaelgpearce/bitcoin-cigs/blob/master/lib/bitcoin_cigs.rb
+    '''
+    curve = G.curve()
+    p = curve.p()
     v,r,s = vrs
     if v < 27 or v >= 35:
         return False
-    cmprs = True if v >= 31 else False
-    pubpairs = possible_public_pairs_for_signature(G, msghash, (r,s))
-    for pair in pubpairs:
-        if addr == public_pair_to_bitcoin_address(pair, cmprs, is_test): return True
-    return False
+    if v >= 31:
+        cmprs = True
+        v -= 4
+    recid = v - 27
+    # 1.1
+    inv_r = numbertheory.inverse_mod(r,N)
+    minus_e = -msghash % N
+    x = (r + (recid // 2) * N) % p
+    # 1.3
+    alpha = ( pow(x,3,p)  + curve.a() * x + curve.b() ) % p
+    beta = numbertheory.modular_sqrt(alpha, p)
+    if (beta - recid) % 2 == 0:
+        y = beta
+    else:
+        y = p - beta
+    # 1.4 the constructor checks that nR is at infinity
+    R = ellipticcurve.Point(curve, x, y, N)
+    # 1.6 compute Q = r^-1 (sR - eG)
+    Q = inv_r * ( s * R + minus_e * G )
+    public_pair = (Q.x(), Q.y())
+    # check that Q is the public key
+    if verify(G, public_pair, msghash, (r, s)):
+        # Checks that addresses match
+        return True if addr == public_pair_to_bitcoin_address(public_pair, cmprs, is_test) else False
+    else:
+        return False
+
 
 '''
 Addresses
